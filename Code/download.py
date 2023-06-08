fe25b6ed87c2318e65daee5179515ec98ab3ba2a
Tajbar-789 <54442879+Tajbar-789@users.noreply.github.com>
2023-06-08 22:18:56 +0530

    Update download.py

--- Code/download.py
+++ Code/download.py
@@ -1,7 +1,12 @@
 import json
 import pandas as pd
 import boto3
+import sys
 
+AWS_ACCESS_KEY_ID=sys.argv[0]
+AWS_SECRET_ACCESS_KEY=sys.argv[1]
+BUCKET_NAME=sys.argv[2]
+KEY=sys.argv[3]
 
-s3_client=boto3.client(service_name='s3',region_name='ap-south-1',aws_access_key_id='AKIAUFGRY5J2WTGZFH45',aws_secret_access_key='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')
-s3_client.download_file(Bucket='sentiment-analysis-project-bucket', Key='json_data.json',Filename='/root/airflow/data/sentiment_analysis_dataset.json')
+s3_client=boto3.client(service_name='s3',region_name='ap-south-1',aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+s3_client.download_file(Bucket=BUCKET_NAME, Key=KEY,Filename='/root/airflow/data/sentiment_analysis_dataset.json')
